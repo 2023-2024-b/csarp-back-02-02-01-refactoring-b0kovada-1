40315dd5c6d940fdd25c3c2cfff2e883f597fd24
b0kovada <kovacshaziadam@gmail.com>
2024-11-12 10:13:06 +0100

    fixup

--- Kreta.Shared/Dtos/StudentDto.cs
+++ Kreta.Shared/Dtos/StudentDto.cs
@@ -3,11 +3,11 @@
 public class StudentDto
 {
     public Guid Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
     public DateTime BirthsDay { get; set; }
     public int SchoolYear { get; set; }
     public SchoolClassType SchoolClass { get; set; }
-    public string EducationLevel { get; set; }
+    public string EducationLevel { get; set; } = string.Empty;
     public bool IsWoomen { get; set; }
 }
